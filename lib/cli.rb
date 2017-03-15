da2dd3a4fe6cf138d542cc122c5b96f3aabf9346
Scott Atkins <satkins16@gmail.com>
2017-03-15 13:51:12 +0000

    began cli call layout

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,7 +1,38 @@
 class CLI
 
   def call
-    puts "Welcome to Rolling Stone's 100 Greatest Guitarists."
+    puts ""
+    puts "WELCOME TO ROLLING STONE'S 100 GREATEST GUITARISTS"
+    puts ""
+    print_list
   end
+
+  def print_list
+    puts "Enter which section you would like to see or enter 'full' for full list:"
+    puts "100-81   80-61   60-41   40-21   20-1"
+    puts ""
+    input = gets.strip
+    case input
+    when "100-81"
+      puts ""
+      puts "that stuff"
+    when "80-61"
+      puts ""
+      puts "that stuff"
+    when "60-41"
+      puts ""
+      puts "that stuff"
+    when "40-21"
+      puts ""
+      puts "that stuff"
+    when "20-1"
+      puts ""
+      puts "that stuff"
+    when "full"
+      puts ""
+      puts "full list"
+    end
+  end
+
 
 end
