1d06a9f3ab63ef256cbf084e700f63e9d42e69b6
Scott Atkins <satkins16@gmail.com>
2017-03-18 16:46:19 +0000

    instantiated all guitarists

--- lib/guitarist.rb
+++ lib/guitarist.rb
@@ -10,4 +10,15 @@ class Guitarist
     @@all << self
   end
 
+  def self.create_guitarists
+    Scraper.create_ranker.each do |guitarist|
+      guitarist_name = guitarist.gsub(/[[:digit:]][\W][\s]/, "")
+      new_guitarist = Guitarist.new(guitarist_name)
+    end
+    @@all.each do |guitarist|
+      guitarist.name = guitarist.name.gsub(/[[:digit:]]/, "")
+    end
+  end
+
+  Guitarist.create_guitarists
 end
